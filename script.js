feab657e8f6f94878439b7d1fd4763804e6caae9
iineb <60065083+iineb@users.noreply.github.com>
2020-06-08 14:55:33 +0100

    UPDATE: Passwords matching

--- script.js
+++ script.js
@@ -16,9 +16,13 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
-function isEmailValid(email) {
+function checkEmail(input) {
     const reg = (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]);
-    return reg.test(String(email).toLowerCase());
+    if (reg.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+       showError(input, 'Email is not valid!');
+    }
 }
 
 function checkRequired(inputArr) {
@@ -31,6 +35,22 @@ function checkRequired(inputArr) {
     });
 }
 
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match!');
+    }
+}
+
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -38,4 +58,8 @@ function getFieldName(input) {
 form.addEventListener('submit', function() {
     e.preventDefault();
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
