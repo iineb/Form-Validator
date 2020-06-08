2274f944a9f84b3cf6755afc03233ae8f2d96f98
iineb <60065083+iineb@users.noreply.github.com>
2020-06-08 14:37:38 +0100

    UPDATE: Email RegEx added

--- script.js
+++ script.js
@@ -4,11 +4,35 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmailValid(email) {
+    const reg = (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]);
+    return reg.test(String(email).toLowerCase());
+}
+
 form.addEventListener('submit', function() {
     e.preventDefault();
     
     if (username.value ==== '') {
         showError(username, 'Username is required!');
+    } else {
+        showSuccess(username);
+    }
+    if (email.value === '') {
+        showError(email, 'Email is required!');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid!');
     } else {
         showSuccess(username);
     }
